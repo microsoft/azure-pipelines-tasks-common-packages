718b615109f051293b8590e6139f1101363410ae
Maksim Petrov <v-mpetrov@microsoft.com>
2023-01-11 15:17:20 +0100

    Resolve comments

--- ci/ci-util.js
+++ ci/ci-util.js
@@ -13,12 +13,23 @@ exports.commonPackagesSourcePath = commonPackagesSourcePath;
 // generic functions
 //------------------------------------------------------------------------------
 
+/**
+ * Parses the content of specified source folder and returns the top-level nested directories
+ * @param {String} source - The path to the source directory
+ * @returns {Array} - The nested directories
+ */
 var getDirectories = source =>
   fs.readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 exports.getDirectories = getDirectories;
 
+/** 
+ * Executes the specified command in system shell and returns the stdout from the command
+ * @param {String} cl - The name or path of the executable file to run
+ * @param {String} inheritStreams - Configure the pipes that are established between the parent and child process. Default value: pipe
+ * @returns {String} - The stdout from the command
+ */
 var run = function (cl, inheritStreams) {
     console.log('');
     console.log(`> ${cl}`);
