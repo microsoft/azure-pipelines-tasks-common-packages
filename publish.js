17a096aef0d5337e951bfad0a3049da2175da3fa
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2023-06-22 09:17:10 +0200

    Generate description for public NPM packages
    - Added logic to generate release notes for common packages
    - Changed the pipeline logic to pass gh token as a secret

--- publish.js
+++ publish.js
@@ -1,25 +1,37 @@
-var fs = require('fs');
-var util = require('./common-npm-packages/build-scripts/util');
+const fs = require('fs');
+const util = require('./common-npm-packages/build-scripts/util');
+const releaseNotes = require('./common-npm-packages/build-scripts/create-release');
 
 console.log('Publishing shared npm packages');
 
-util.cd('common-npm-packages');
-fs.readdirSync('./', { encoding: 'utf-8' }).forEach(child => {
-    if (fs.statSync(child).isDirectory() &&  ['build-scripts', '.git', '_download', 'node_modules'].indexOf(child) < 0) {
-        console.log('\n----------------------------------');
-        console.log(child);
-        console.log('----------------------------------');
-        util.cd(child);
-        util.cd('_build');
-        try {
-            const npmrc = `//registry.npmjs.org/:_authToken=\${NPM_TOKEN}`;
-            console.log(`Writing .npmrc: ${npmrc}`);
-            fs.writeFileSync('.npmrc', npmrc);
-            util.run('npm publish');
+async function publishPackages(packages) {
+    for (let i = 0; i < packages.length; i++) {
+        const package = packages[i];
+        if (fs.statSync(package).isDirectory() &&  ['build-scripts', '.git', '_download', 'node_modules'].indexOf(package) < 0) {
+            console.log('\n----------------------------------');
+            console.log(package);
+            console.log('----------------------------------');
+            util.cd(package);
+            util.cd('_build');
+            try {
+                const npmrc = `//registry.npmjs.org/:_authToken=\${NPM_TOKEN}`;
+                console.log(`Writing .npmrc: ${npmrc}`);
+                fs.writeFileSync('.npmrc', npmrc);
+                util.run('npm publish');
+                await releaseNotes.createReleaseNotes(package, 'main');
+            }
+            catch(ex) {
+                if (ex instanceof util.CreateReleaseError) {
+                    console.log(`Error creating release notes: ${ex.message}`);                    
+                } else {
+                    console.log('Publish failed - this usually indicates that the package has already been published');
+                }
+            }
+            util.cd('../..');
         }
-        catch(ex) {
-            console.log('Publish failed - this usually indicates that the package has already been published');
-        }
-        util.cd('../..');
     }
-});
+}
+
+util.cd('common-npm-packages');
+var packages = fs.readdirSync('./', { encoding: 'utf-8' });
+publishPackages(packages);
