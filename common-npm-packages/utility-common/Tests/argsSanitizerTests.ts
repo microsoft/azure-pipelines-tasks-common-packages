aea571fa9b87ad655280a86530c88b1e9b12790b
MerlynOMsft <44985659+merlynomsft@users.noreply.github.com>
2023-08-01 02:24:10 -0700

    Fix for regex error (#200)
    * Fix for regex error
    
    * add package-lock.json
    
    * Fix escaping symbols
    
    * Make sanitizer as pure function
    
    * Add removed symbols dictionary to telemetry
    
    * Update naming
    
    * Add global flag check test
    
    * Add empty check
    
    * Specify matches type
    
    * Update jsdoc
    
    * simplify name
    
    ---------
    
    Co-authored-by: Konstantin Tyukalov <v-ktyukalov@microsoft.com>

--- common-npm-packages/utility-common/Tests/argsSanitizerTests.ts
+++ common-npm-packages/utility-common/Tests/argsSanitizerTests.ts
@@ -1,5 +1,5 @@
 import assert = require("assert");
-import { sanitizeScriptArgs } from "../argsSanitizer"
+import { sanitizeArgs } from "../argsSanitizer"
 
 export function runArgsSanitizerTests() {
     ([
@@ -7,12 +7,43 @@ export function runArgsSanitizerTests() {
         "Param1 `| Out-File ./321",
         "'Param 1' 'Param 2'",
         "hello`;world",
+        null,
+        undefined,
+        ""
     ] as string[]).forEach((input) => {
-        it(`Should process '${input}' with no replacement.`, () => {
+        it(`Should return '${input}' with no replacement.`, () => {
 
-            const result = sanitizeScriptArgs(input, { argsSplitSymbols: '``', telemetryFeature: '', warningLocSymbol: '' });
+            const [result] = sanitizeArgs(input, { argsSplitSymbols: '``' });
 
             assert.equal(result, input);
+        })
+    });
+
+    ([
+        "Param1 Param2",
+        "Param1 \\| Out-File ./321",
+        "'Param 1' 'Param 2'",
+        "hello`\\;world",
+    ] as string[]).forEach((input) => {
+        it(`Should return '${input}' with no replacement. With \\`, () => {
+
+            const [result] = sanitizeArgs(input, { argsSplitSymbols: '\\\\', });
+
+            assert.equal(result, input);
+        })
+    });
+
+    ([
+        ["1 ``; whoami", "``", "1 ``_#removed#_ whoami"],
+        ["'1 ``; whoami'", "\\\\", "'1 ``_#removed#_ whoami'"], // we're ignoring quotes
+        ["1 \\\\; whoami", "\\\\", "1 \\\\_#removed#_ whoami"],
+        ["1 `; whoami", "\\\\", "1 `_#removed#_ whoami"] // if trying to use not matched escaping symbol
+    ] as [string, '\\\\' | '``', string][]).forEach(([input, argsSplitSymbols, expected]) => {
+        it(`Should process '${input}' and replace to '${expected}'`, () => {
+
+            const [result] = sanitizeArgs(input, { argsSplitSymbols });
+
+            assert.equal(result, expected);
         })
     });
 
@@ -24,19 +55,56 @@ export function runArgsSanitizerTests() {
     ] as [string, string][]).forEach(([input, expected]) => {
         it(`'${input}' should be replaced to '${expected}'.`, () => {
 
-            const result = sanitizeScriptArgs(input, { argsSplitSymbols: '``', telemetryFeature: '', warningLocSymbol: '' });
+            const [result] = sanitizeArgs(input, { argsSplitSymbols: '``' });
 
             assert.equal(result, expected);
         })
     });
 
     it('Should use input reg exp', () => {
-        const regx = /2/;
+        const regx = /2/g;
         const input = "1 2";
         const expected = '1 _#removed#_';
 
-        const result = sanitizeScriptArgs(input, { argsSplitSymbols: '``', telemetryFeature: '', warningLocSymbol: '', saniziteRegExp: regx });
+        const [result] = sanitizeArgs(input, { argsSplitSymbols: '``', saniziteRegExp: regx });
 
         assert.equal(result, expected);
     });
+
+    it('Throws error if \'global\' flag not set', () => {
+        const regx = /1/;
+
+        assert.throws(() => sanitizeArgs('1', { argsSplitSymbols: '``', saniziteRegExp: regx }));
+    });
+}
+
+export function runArgsSanitizerTelemetryTests() {
+    it('Should return correct telemetry', () => {
+        const regx = /[^2 ]/g;
+        const input = "1 2 4 3 3 3;;_";
+        const expected = {
+            removedSymbols: {
+                '1': 1,
+                '3': 3,
+                '4': 1,
+                ';': 2,
+                '_': 1,
+            },
+            removedSymbolsCount: 8
+        };
+
+        const [, result] = sanitizeArgs(input, { argsSplitSymbols: '``', saniziteRegExp: regx });
+
+        assert.deepStrictEqual(result, expected);
+    })
+
+    it('Returns null if nothing changed', () => {
+        const regx = /2/g;
+        const input = "1 3 5";
+        const expected = null
+
+        const [, result] = sanitizeArgs(input, { argsSplitSymbols: '``', saniziteRegExp: regx });
+
+        assert.deepStrictEqual(result, expected);
+    })
 }
