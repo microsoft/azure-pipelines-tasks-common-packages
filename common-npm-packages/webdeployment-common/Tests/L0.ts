d3e480158418d39c56a21f9927f162826d2816bc
Ondrej Netocny <onetocny@microsoft.com>
2023-10-20 12:11:53 +0200

    Add ParameterParserUtility

--- common-npm-packages/webdeployment-common/Tests/L0.ts
+++ common-npm-packages/webdeployment-common/Tests/L0.ts
@@ -7,6 +7,7 @@ import { runL1JSONVarSubWithCommentsTests } from "./L1JSONVarSubWithComments";
 import { runL1JsonVarSubTests } from "./L1JsonVarSub";
 import { runL1JsonVarSubV2Tests } from "./L1JsonVarSubV2";
 import { runL1ValidateFileEncodingTests } from "./L1ValidateFileEncoding";
+import { runParameterParserUtilityTests } from "./L0ParameterParserUtility";
 
 describe('Web deployment common tests', () => {
     describe('GetMSDeployCmdArgs tests', runGetMSDeployCmdArgsTests);
@@ -19,4 +20,5 @@ describe('Web deployment common tests', () => {
     describe("L1JsonVarSub tests", runL1JsonVarSubTests);
     describe("L1JsonVarSubV2 tests", runL1JsonVarSubV2Tests);
     describe("L1ValidateFileEncoding tests", runL1ValidateFileEncodingTests);
+    describe("ParameterParserUtility tests", runParameterParserUtilityTests);
 });
