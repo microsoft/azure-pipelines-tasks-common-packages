7230ae3f91b1dbe6812897950eaad461b41a15de
tintse-thxsky-MSFT <104647196+tintse-thxsky-MSFT@users.noreply.github.com>
2024-06-14 16:50:09 +0000

    fix azure-pipelines-tasks-packaging-common repo url. mask auth token in locationUltilities.ts fixed some vulnerability by running npm audit fix

--- common-npm-packages/packaging-common/locationUtilities.ts
+++ common-npm-packages/packaging-common/locationUtilities.ts
@@ -116,7 +116,8 @@ export function getSystemAccessToken(): string {
     tl.debug('Getting credentials for local feeds');
     const auth = tl.getEndpointAuthorization('SYSTEMVSSCONNECTION', false);
     if (auth.scheme === 'OAuth') {
-        tl.debug('Got auth token');
+        tl.debug('Got auth token, setting it as secret so it does not print in console log');
+        tl.setSecret(auth.parameters['AccessToken']);
         return auth.parameters['AccessToken'];
     } else {
         tl.warning('Could not determine credentials to use');
