f2386a0c5be14988d8a4e462976e297a381f9763
Dmitrii Bobreshev (Akvelon INC) <v-bobreshevd@microsoft.com>
2024-08-26 10:13:28 +0200

    Increase timout for tests

--- common-npm-packages/azure-arm-rest/Tests/L0.ts
+++ common-npm-packages/azure-arm-rest/Tests/L0.ts
@@ -3,9 +3,12 @@ import { KuduServiceTests } from "./L0-azure-arm-app-service-kudu-tests";
 import { ApplicationInsightsTests } from "./L0-azure-arm-appinsights-tests";
 import { ApplicationInsightsTests as ApplicationInsightsTestsWebTests } from "./L0-azure-arm-appinsights-webtests-tests";
 import { ResourcesTests } from "./L0-azure-arm-resource-tests";
-
+const DEFAULT_TIMEOUT = 1000 * 20;
 
 describe("AzureARMRestTests", () => {
+    beforeEach(function () {
+        this.timeout(DEFAULT_TIMEOUT);
+    });
     describe("KuduService tests", KuduServiceTests)
     describe("AzureAppServiceMock tests", AzureAppServiceMockTests)
     describe("ApplicationInsights tests", ApplicationInsightsTests)
