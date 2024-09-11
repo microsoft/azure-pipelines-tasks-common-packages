c4e229a55827f6b8c249f8672f670737249ff09c
v-mazayt <v-mazayt@microsoft.com>
2024-09-11 14:01:27 +0200

    Check FF

--- common-npm-packages/codecoverage-tools/jacoco/jacoco.gradle.ccenabler.ts
+++ common-npm-packages/codecoverage-tools/jacoco/jacoco.gradle.ccenabler.ts
@@ -26,13 +26,16 @@ export class JacocoGradleCodeCoverageEnabler extends cc.JacocoCodeCoverageEnable
         let reportDir = ccProps["reportdirectory"];
         let gradle5xOrHigher = ccProps["gradle5xOrHigher"] && ccProps["gradle5xOrHigher"] === "true";
         let codeCoveragePluginData = null;
-        let gradleVersion = ccProps["gradleVersion"];
+        const gradleVersion = ccProps["gradleVersion"];
+        const useJacocoTemplateV2forSingleModule = ccProps['useJacocoTemplateV2forSingleModule'] && ccProps['useJacocoTemplateV2forSingleModule'] === 'true';
+        const useJacocoTemplateV2forMultiModule = ccProps['useJacocoTemplateV2forMultiModule'] && ccProps['useJacocoTemplateV2forMultiModule'] === 'true';
+        const useTemplateV2FFisEnabled = isMultiModule && useJacocoTemplateV2forMultiModule || !isMultiModule && useJacocoTemplateV2forSingleModule;
 
         let filter = _this.extractFilters(classFilter);
         let jacocoExclude = _this.applyFilterPattern(filter.excludeFilter);
         let jacocoInclude = _this.applyFilterPattern(filter.includeFilter);
 
-        const jacocoGradleEnabler = this.getJacocoGradleEnablerFunction(isMultiModule, gradleVersion);
+        const jacocoGradleEnabler = _this.getJacocoGradleEnablerFunction(isMultiModule, gradleVersion, useTemplateV2FFisEnabled);
         codeCoveragePluginData = jacocoGradleEnabler(jacocoExclude.join(","), jacocoInclude.join(","), classFileDirs, reportDir, gradle5xOrHigher);
 
         try {
@@ -68,10 +71,12 @@ export class JacocoGradleCodeCoverageEnabler extends cc.JacocoCodeCoverageEnable
     * @returns The appropriate Jacoco Gradle enabler function.
     * @throws An error if the Gradle version or module type is not supported.
     */
-    private getJacocoGradleEnablerFunction(isMultiModule: boolean, gradleVersion: string) {
+    private getJacocoGradleEnablerFunction(isMultiModule: boolean, gradleVersion: string, useTemplateV2FFisEnabled: boolean) {
         const modeleType = isMultiModule ? "multi" : "single";
         const resolvedGradleVersion = semver.valid(semver.coerce(gradleVersion));
-        const templateVersion = resolvedGradleVersion && semver.gte(resolvedGradleVersion, '6.1.0') ? "V2" : "V1";
+
+        // if gradle version is 'unknown' or below 6.1.0, use V1 template
+        const templateVersion = resolvedGradleVersion && semver.gte(resolvedGradleVersion, '6.1.0') && useTemplateV2FFisEnabled ? "V2" : "V1";
 
         tl.debug(`Gradle module type: ${modeleType}, Gradle version: ${gradleVersion}, ResolvedGradleVersion: ${resolvedGradleVersion} Template version: ${templateVersion}`)
 
