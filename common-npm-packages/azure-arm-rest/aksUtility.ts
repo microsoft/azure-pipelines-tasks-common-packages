b9f79e8ced3170b0345a4f5011909ddc078bd80e
audrastump <a.e.stump@wustl.edu>
2025-02-21 10:31:28 -0800

    refactored utility function

--- common-npm-packages/azure-arm-rest/aksUtility.ts
+++ common-npm-packages/azure-arm-rest/aksUtility.ts
@@ -4,37 +4,33 @@ import { AzureRMEndpoint } from './azure-arm-endpoint';
 import { AzureEndpoint, AKSClusterAccessProfile, AKSCredentialResult} from './azureModels';
 
 
-async function getKubeConfigFromAKS(azureSubscriptionEndpoint: string, resourceGroup: string, clusterName: string, useClusterAdmin?: boolean) : Promise<string> {
+
+async function getKubeConfigFromAKS(azureSubscriptionEndpoint: string, resourceGroup: string, name: string, isFleet: boolean, useClusterAdmin?: boolean): Promise<string> {
     const azureEndpoint: AzureEndpoint = await (new AzureRMEndpoint(azureSubscriptionEndpoint)).getEndpoint();
     const aks = new AzureAksService(azureEndpoint);
-    const USE_AKS_CREDENTIAL_API = tl.getBoolFeatureFlag('USE_AKS_CREDENTIAL_API');
-    tl.debug(tl.loc("KubernetesClusterResourceGroup", clusterName, resourceGroup));
+    tl.debug(tl.loc("KubernetesClusterResourceGroup", name, resourceGroup));
     let base64Kubeconfig;
-    if (USE_AKS_CREDENTIAL_API) {
-        let clusterInfo : AKSCredentialResult = await aks.getClusterCredential(resourceGroup, clusterName, useClusterAdmin);
+    if (isFleet) {
+        let clusterInfo: AKSCredentialResult = await aks.getFleetCredential(resourceGroup, name);
         base64Kubeconfig = Buffer.from(clusterInfo.value, 'base64');
     } else {
-        let clusterInfo : AKSClusterAccessProfile = await aks.getAccessProfile(resourceGroup, clusterName, useClusterAdmin);
-        base64Kubeconfig = Buffer.from(clusterInfo.properties.kubeConfig, 'base64');
+        const USE_AKS_CREDENTIAL_API = tl.getBoolFeatureFlag('USE_AKS_CREDENTIAL_API');
+        if (USE_AKS_CREDENTIAL_API) {
+            let clusterInfo: AKSCredentialResult = await aks.getClusterCredential(resourceGroup, name, useClusterAdmin);
+            base64Kubeconfig = Buffer.from(clusterInfo.value, 'base64');
+        } else {
+            let clusterInfo: AKSClusterAccessProfile = await aks.getAccessProfile(resourceGroup, name, useClusterAdmin);
+            base64Kubeconfig = Buffer.from(clusterInfo.properties.kubeConfig, 'base64');
+        }
     }
-    return base64Kubeconfig.toString();
-}
-
-async function getKubeConfigFromFleet(azureSubscriptionEndpoint: string, resourceGroup: string, fleetName: string) : Promise<string> {
-    const azureEndpoint: AzureEndpoint = await (new AzureRMEndpoint(azureSubscriptionEndpoint)).getEndpoint();
-    const aks = new AzureAksService(azureEndpoint);
-    tl.debug(tl.loc("KubernetesClusterResourceGroup", fleetName, resourceGroup));
-    let base64Kubeconfig;
-    let clusterInfo : AKSCredentialResult = await aks.getFleetCredential(resourceGroup, fleetName);
-    base64Kubeconfig = Buffer.from(clusterInfo.value, 'base64');
     return base64Kubeconfig.toString();
 }
 
 
 export async function getKubeConfig(azureSubscriptionEndpoint, resourceGroup, clusterName, useClusterAdmin): Promise<string> {
-    return getKubeConfigFromAKS(azureSubscriptionEndpoint, resourceGroup, clusterName, useClusterAdmin);
+    return getKubeConfigFromAKS(azureSubscriptionEndpoint, resourceGroup, clusterName, false, useClusterAdmin);
 }
 
 export async function getKubeConfigForFleet(azureSubscriptionEndpoint, resourceGroup, fleetName): Promise<string> {
-    return getKubeConfigFromFleet(azureSubscriptionEndpoint, resourceGroup, fleetName);
+    return getKubeConfigFromAKS(azureSubscriptionEndpoint, resourceGroup, fleetName, true);
 }
