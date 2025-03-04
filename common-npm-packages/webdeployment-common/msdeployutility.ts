1beba296257e5a306d5ec047d731c88a33e0e426
D Kodanda Ramudu (LTIMINDTREE LIMITED) <v-dko@microsoft.com>
2025-03-04 15:02:42 +0530

    modified featureflag conditions

--- common-npm-packages/webdeployment-common/msdeployutility.ts
+++ common-npm-packages/webdeployment-common/msdeployutility.ts
@@ -113,15 +113,15 @@ export function getMSDeployCmdArgs(webAppPackage: string, webAppName: string, pr
 function escapeQuotes(additionalArguments: string): string {
     const parsedArgs = parseAdditionalArguments(additionalArguments);
     const separator = ",";
-    let USE_COMMA_SEPERATOR = tl.getBoolFeatureFlag('USE_COMMA_SEPERATOR');
-
+    let CommaSeperatedCSEnabled = tl.getPipelineFeature('CommaSeperatedConnectionString');
     const formattedArgs = parsedArgs.map(function (arg) {
         let formattedArg = '';
         let equalsSignEncountered = false;
         for (let i = 0; i < arg.length; i++) {
-            const char = arg.charAt(i);           
-            let USE_COMMA_SEPERATORS=USE_COMMA_SEPERATOR ? (char == separator && equalsSignEncountered && ((formattedArg.startsWith("'") && formattedArg.endsWith("'")) || (formattedArg.startsWith('"') && formattedArg.endsWith('"')))): (char == separator && equalsSignEncountered);
-            if (USE_COMMA_SEPERATORS) {
+            const char = arg.charAt(i); 
+            let quotedStringCheck =  (char == separator && equalsSignEncountered && ((formattedArg.startsWith("'") && formattedArg.endsWith("'")) || (formattedArg.startsWith('"') && formattedArg.endsWith('"'))));     
+            let CommaSeperatorCheck=CommaSeperatedCSEnabled ? quotedStringCheck : (char == separator && equalsSignEncountered);
+            if (CommaSeperatorCheck) {
                 equalsSignEncountered = false;
                 arg = arg.replace(formattedArg, escapeArg(formattedArg));
                 formattedArg = '';
