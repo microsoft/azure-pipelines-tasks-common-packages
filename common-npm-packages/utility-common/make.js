943d5f5f04c7a176f85441926c624df190311588
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2025-05-23 17:26:28 +0200

    Update downloading 7zip from urls

--- common-npm-packages/utility-common/make.js
+++ common-npm-packages/utility-common/make.js
@@ -1,38 +1,34 @@
-const path = require('path');
-const fs = require('fs');
+const { existsSync } = require('node:fs');
+const { join } = require('node:path');
 
 const util = require('../build-scripts/util');
 const { downloadArchive } = require('../build-scripts/downloadArchive');
 
 const buildPath = './_build';
 const toolPath = './tools';
-const zipUrl5 = 'https://vstsagenttools.blob.core.windows.net/tools/7zip/5/7zip.zip';
-const zipUrl24 = 'https://vstsagenttools.blob.core.windows.net/tools/7zip/24.09/7zip.zip';
 
-// Download and extract 7zip version 5
-const targetPath5 = downloadArchive(zipUrl5, path.join('../_download', toolPath, '7zip5'));
+const zipUrls = {
+    '7zip5': 'https://vstsagenttools.blob.core.windows.net/tools/7zip/5/7zip.zip',
+    '7zip24': 'https://vstsagenttools.blob.core.windows.net/tools/7zip/24.09/7zip.zip'
+};
 
-if (!fs.existsSync(path.join(toolPath, '7zip5'))) {
-    util.mkdir('-p', path.join(toolPath, '7zip5'));
+for (const [version, url] of Object.entries(zipUrls)) {
+    const targetPath = join(toolPath, version);
+    const sourcePath = downloadArchive(url, join('../_download', targetPath));
+
+    if (!existsSync(targetPath)) {
+        util.mkdir('-p', targetPath);
+    }
+
+    util.cp('-rf', join(sourcePath, '*'), targetPath);
 }
-
-util.cp('-rf', path.join(targetPath5, '*'), path.join(toolPath, '7zip5'));
-
-// Download and extract 7zip version 24.09
-const targetPath24 = downloadArchive(zipUrl24, path.join('../_download', toolPath, '7zip24'));
-
-if (!fs.existsSync(path.join(toolPath, '7zip24'))) {
-    util.mkdir('-p', path.join(toolPath, '7zip24'));
-}
-
-util.cp('-rf', path.join(targetPath24, '*'), path.join(toolPath, '7zip24'));
 
 util.rm('-rf', buildPath)
-util.run(path.join(__dirname, 'node_modules/.bin/tsc') + ' --outDir ' + buildPath);
+util.run(join(__dirname, 'node_modules/.bin/tsc') + ' --outDir ' + buildPath);
 
-util.cp(path.join(__dirname, 'package.json'), buildPath);
-util.cp(path.join(__dirname, 'package-lock.json'), buildPath);
-util.cp(path.join(__dirname, 'module.json'), buildPath);
+util.cp(join(__dirname, 'package.json'), buildPath);
+util.cp(join(__dirname, 'package-lock.json'), buildPath);
+util.cp(join(__dirname, 'module.json'), buildPath);
 util.cp('-r', 'tools', buildPath);
 util.cp('-r', 'Strings', buildPath);
 util.cp('-r', 'node_modules', buildPath);
