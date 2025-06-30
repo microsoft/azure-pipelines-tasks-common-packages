98ed3d82dced2e585612cb6d667444d227e82290
Ivan Duplenskikh <115665590+ivanduplenskikh@users.noreply.github.com>
2025-06-30 13:08:23 +0200

    Migrate build scripts to typescript project

--- ci/verify-source-changes.js
+++ ci/verify-source-changes.js
@@ -1,29 +1,32 @@
-var path = require('path');
-var process = require("process");
-var util = require('./ci-util');
+const path = require('node:path');
+const process = require("node:process");
 
-packagesList = util.getDirectories(util.commonPackagesSourcePath);
+const util = require('./ci-util');
 
-var totalDiffList = [];
+const packagesList = util.getDirectories(util.commonPackagesSourcePath);
+
+/** @type {string[]} */
+const totalDiffList = [];
 
 // Due to migration scope, some of the packages were migrated without the changes, which cause the check failures
 // We should remove the exclusions after the related fixes are done
-var excludeList = ["common-npm-packages/kubernetes-common/_tools/7zip/License.txt"];
+const excludeList = ["common-npm-packages/kubernetes-common/_tools/7zip/License.txt"];
 
 console.log(`Checking packages sources for uncommitted changes...`);
+
 packagesList.forEach(function(packageName) {
     console.log(`====================${packageName}====================`);
 
-    var packageSourcePath = path.join(util.commonPackagesSourcePath, packageName);
+    const packageSourcePath = path.join(util.commonPackagesSourcePath, packageName);
 
-    var diffString = util.run(`git diff --name-only ${packageSourcePath}`);
-    var diffList = diffString.split("\n").filter(Boolean);
+    const diffString = util.run(`git diff --name-only ${packageSourcePath}`);
+    const diffList = diffString.split("\n").filter(Boolean);
 
     if (diffList.length) {
         console.log(``);
         console.log(`Uncommitted changes found:`);
         console.log(``);
-        diffList.forEach(function(item){
+        diffList.forEach(function(item) {
             if (excludeList.includes(item)) {
                 console.log(` - ${item} - is in the exclusion list`);
                 return;
@@ -44,7 +47,7 @@ if (totalDiffList.length) {
     console.log(`Please build packages locally and commit specified changes:`);
     console.log(``);
 
-    totalDiffList.forEach(function(item){
+    totalDiffList.forEach(function(item) {
         console.log(` - ${item}`);
     });
 
