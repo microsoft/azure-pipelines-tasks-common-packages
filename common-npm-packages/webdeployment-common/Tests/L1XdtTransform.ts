773fe53e5e98158b07126049f98bf7d74b14725c
nemanjarogic <nemanjarogic93@gmail.com>
2026-08-06 16:38:30 +0200

    Fix security issue - initial iteration

--- common-npm-packages/webdeployment-common/Tests/L1XdtTransform.ts
+++ common-npm-packages/webdeployment-common/Tests/L1XdtTransform.ts
@@ -11,6 +11,9 @@ import { detectFileEncoding } from "../fileencoding";
 export function runL1XdtTransformTests(this: Mocha.Suite) {
 
     this.timeout(parseInt(process.env.TASK_TEST_TIMEOUT) || 20000);
+    const temporaryTransformFiles: string[] = [];
+
+    tl.setResourcePath(path.join(__dirname, '..', 'module.json'));
 
     beforeEach(done => {
         tl.cp(getAbsolutePath('Web.config'), getAbsolutePath('Web_test.config'), '-f', false);
@@ -21,6 +24,8 @@ export function runL1XdtTransformTests(this: Mocha.Suite) {
     afterEach(done => {
         try {
             tl.rmRF(getAbsolutePath('Web_test.config'));
+            temporaryTransformFiles.forEach(transformFile => tl.rmRF(transformFile));
+            temporaryTransformFiles.length = 0;
         }
         catch (error) {
             tl.debug(error);
@@ -44,8 +49,85 @@ export function runL1XdtTransformTests(this: Mocha.Suite) {
 
     });
 
+    it('Rejects XDT imports that load assemblies by path', function(done: Mocha.Done) {
+        const transformFile = writeTemporaryTransformFile('Web.BlockedImportPath.config',
+            '<configuration xmlns:xdt="http://schemas.microsoft.com/XML-Document-Transform">\r\n' +
+            '  <xdt:Import path="CustomTransform.dll" namespace="CustomTransform" />\r\n' +
+            '  <appSettings xdt:Transform="SetAttributes" />\r\n' +
+            '</configuration>\r\n');
+
+        assert.throws(
+            () => applyXdtTransformation(getAbsolutePath('Web_test.config'), transformFile),
+            /xdt:Import/,
+            'Should reject transform files that import assemblies by path');
+        done();
+    });
+
+    it('Rejects XDT imports that load assemblies by name', function(done: Mocha.Done) {
+        const transformFile = writeTemporaryTransformFile('Web.BlockedImportAssembly.config',
+            '<configuration xmlns:xdt="http://schemas.microsoft.com/XML-Document-Transform">\r\n' +
+            '  <xdt:Import assembly="CustomTransform" namespace="CustomTransform" />\r\n' +
+            '  <appSettings xdt:Transform="SetAttributes" />\r\n' +
+            '</configuration>\r\n');
+
+        assert.throws(
+            () => applyXdtTransformation(getAbsolutePath('Web_test.config'), transformFile),
+            /xdt:Import/,
+            'Should reject transform files that import assemblies by name');
+        done();
+    });
+
+    it('Rejects XDT imports declared with alternate namespace prefixes', function(done: Mocha.Done) {
+        const transformFile = writeTemporaryTransformFile('Web.BlockedImportAlias.config',
+            '<configuration xmlns:customXdt="http://schemas.microsoft.com/XML-Document-Transform">\r\n' +
+            '  <customXdt:Import path="CustomTransform.dll" namespace="CustomTransform" />\r\n' +
+            '  <appSettings customXdt:Transform="SetAttributes" />\r\n' +
+            '</configuration>\r\n');
+
+        assert.throws(
+            () => applyXdtTransformation(getAbsolutePath('Web_test.config'), transformFile),
+            /xdt:Import/,
+            'Should reject transform files that import assemblies with alternate namespace prefixes');
+        done();
+    });
+
+    it('Rejects custom XDT transform types', function(done: Mocha.Done) {
+        const transformFile = writeTemporaryTransformFile('Web.BlockedTransform.config',
+            '<configuration xmlns:xdt="http://schemas.microsoft.com/XML-Document-Transform">\r\n' +
+            '  <appSettings xdt:Transform="Probe" />\r\n' +
+            '</configuration>\r\n');
+
+        assert.throws(
+            () => applyXdtTransformation(getAbsolutePath('Web_test.config'), transformFile),
+            /Probe/,
+            'Should reject custom XDT transform types');
+        done();
+    });
+
+    it('Rejects custom XDT locator types', function(done: Mocha.Done) {
+        const transformFile = writeTemporaryTransformFile('Web.BlockedLocator.config',
+            '<configuration xmlns:xdt="http://schemas.microsoft.com/XML-Document-Transform">\r\n' +
+            '  <appSettings>\r\n' +
+            '    <add key="Custom" value="Custom" xdt:Locator="Probe(key)" xdt:Transform="SetAttributes" />\r\n' +
+            '  </appSettings>\r\n' +
+            '</configuration>\r\n');
+
+        assert.throws(
+            () => applyXdtTransformation(getAbsolutePath('Web_test.config'), transformFile),
+            /Probe/,
+            'Should reject custom XDT locator types');
+        done();
+    });
+
     function getAbsolutePath(file: string): string {
         return path.join(__dirname, 'L1XdtTransform', file);
+    }
+
+    function writeTemporaryTransformFile(file: string, content: string): string {
+        const transformFile = getAbsolutePath(file);
+        fs.writeFileSync(transformFile, content);
+        temporaryTransformFiles.push(transformFile);
+        return transformFile;
     }
 
     function readXmlFile(path: string): ltx.Element {
--- common-npm-packages/webdeployment-common/xdttransformationutility.ts
+++ common-npm-packages/webdeployment-common/xdttransformationutility.ts
@@ -1,5 +1,28 @@
 import tl = require('azure-pipelines-task-lib/task');
+import * as fs from 'fs';
 import path = require('path');
+import { DOMParser } from '@xmldom/xmldom';
+
+const xdtNamespace = 'http://schemas.microsoft.com/XML-Document-Transform';
+const builtInXdtTransformTypes = [
+    'Insert',
+    'InsertAfter',
+    'InsertBefore',
+    'InsertIfMissing',
+    'Remove',
+    'RemoveAll',
+    'RemoveAttributes',
+    'Replace',
+    'SetAttributes',
+    'SetTokenizedAttributeStorage',
+    'SetTokenizedAttributes'
+];
+const builtInXdtLocatorTypes = [
+    'Condition',
+    'DefaultLocator',
+    'Match',
+    'XPath'
+];
 
 export function expandWildcardPattern(folderPath: string, wildcardPattern : string) {
     var matchingFiles = tl.findMatch(folderPath, wildcardPattern,   { followSymbolicLinks: false, allowBrokenSymbolicLinks: false, followSpecifiedSymbolicLink: false });
@@ -21,6 +44,8 @@ export function expandWildcardPattern(folderPath: string, wildcardPattern : stri
 */
 export function applyXdtTransformation(sourceFile: string, transformFile: string, destinationFile?: string) {
 
+    validateXdtTransformFile(transformFile);
+
     var cttPath = path.join(__dirname, "ctt", "ctt", "ctt.exe"); 
     var cttArgsArray= [
         "s:" + sourceFile,
@@ -36,6 +61,157 @@ export function applyXdtTransformation(sourceFile: string, transformFile: string
     if(cttExecutionResult.stderr) {
         throw new Error(tl.loc("XdtTransformationErrorWhileTransforming", sourceFile, transformFile));
     }
+}
+
+function validateXdtTransformFile(transformFile: string): void {
+    const xmlContent = readTransformFile(transformFile);
+    const transformDocument = parseTransformFile(transformFile, xmlContent);
+    validateXdtNode(transformFile, transformDocument.documentElement);
+}
+
+function readTransformFile(transformFile: string): string {
+    const buffer = fs.readFileSync(transformFile);
+    if (buffer.length >= 4 && buffer.slice(0, 4).equals(Buffer.from([255, 254, 0, 0]))) {
+        throw new Error(tl.loc('XdtTransformationUnsupportedEncoding', transformFile, 'UTF-32LE'));
+    }
+
+    if (buffer.length >= 4 && buffer.slice(0, 4).equals(Buffer.from([0, 0, 254, 255]))) {
+        throw new Error(tl.loc('XdtTransformationUnsupportedEncoding', transformFile, 'UTF-32BE'));
+    }
+
+    if (buffer.length >= 3 && buffer.slice(0, 3).equals(Buffer.from([239, 187, 191]))) {
+        return buffer.toString('utf8');
+    }
+
+    if (buffer.length >= 2 && buffer.slice(0, 2).equals(Buffer.from([255, 254]))) {
+        return buffer.toString('utf16le');
+    }
+
+    if (buffer.length >= 2 && buffer.slice(0, 2).equals(Buffer.from([254, 255]))) {
+        throw new Error(tl.loc('XdtTransformationUnsupportedEncoding', transformFile, 'UTF-16BE'));
+    }
+
+    if (buffer.length >= 4) {
+        var typeCode = 0;
+        for (let index = 0; index < 4; index++) {
+            typeCode = typeCode << 1;
+            typeCode = typeCode | (buffer[index] > 0 ? 1 : 0);
+        }
+
+        if (typeCode == 10) {
+            return buffer.toString('utf16le');
+        }
+
+        if (typeCode == 5) {
+            throw new Error(tl.loc('XdtTransformationUnsupportedEncoding', transformFile, 'UTF-16BE'));
+        }
+
+        if (typeCode == 1) {
+            throw new Error(tl.loc('XdtTransformationUnsupportedEncoding', transformFile, 'UTF-32BE'));
+        }
+
+        if (typeCode == 8) {
+            throw new Error(tl.loc('XdtTransformationUnsupportedEncoding', transformFile, 'UTF-32LE'));
+        }
+    }
+
+    return buffer.toString('utf8');
+}
+
+function parseTransformFile(transformFile: string, xmlContent: string): Document {
+    try {
+        return new DOMParser({
+            errorHandler: {
+                warning: function(message) {
+                    tl.debug(message);
+                },
+                error: function(message) {
+                    throw new Error(message);
+                },
+                fatalError: function(message) {
+                    throw new Error(message);
+                }
+            }
+        }).parseFromString(xmlContent, 'text/xml');
+    }
+    catch (error) {
+        throw new Error(tl.loc('XdtTransformationInvalidXml', transformFile, error.message || error));
+    }
+}
+
+function validateXdtNode(transformFile: string, node: Node): void {
+    if (!node) {
+        return;
+    }
+
+    if (node.nodeType == 1) {
+        const element = node as Element;
+        validateXdtElement(transformFile, element);
+        validateXdtAttributes(transformFile, element);
+    }
+
+    if (!node.childNodes) {
+        return;
+    }
+
+    for (let index = 0; index < node.childNodes.length; index++) {
+        validateXdtNode(transformFile, node.childNodes.item(index));
+    }
+}
+
+function validateXdtElement(transformFile: string, element: Element): void {
+    if (isXdtNode(element, 'Import')) {
+        throw new Error(tl.loc('XdtTransformationBlockedImport', transformFile));
+    }
+}
+
+function validateXdtAttributes(transformFile: string, element: Element): void {
+    if (!element.attributes) {
+        return;
+    }
+
+    for (let index = 0; index < element.attributes.length; index++) {
+        const attribute = element.attributes.item(index);
+        if (!attribute || attribute.namespaceURI != xdtNamespace) {
+            continue;
+        }
+
+        const attributeName = getLocalName(attribute);
+        if (attributeName == 'Transform') {
+            validateBuiltInXdtType(transformFile, attributeName, attribute.value, builtInXdtTransformTypes);
+        }
+        else if (attributeName == 'Locator') {
+            validateBuiltInXdtType(transformFile, attributeName, attribute.value, builtInXdtLocatorTypes);
+        }
+    }
+}
+
+function validateBuiltInXdtType(transformFile: string, attributeName: string, attributeValue: string, builtInTypes: string[]): void {
+    const typeName = getXdtTypeName(attributeValue);
+    if (!typeName || builtInTypes.indexOf(typeName) != -1) {
+        return;
+    }
+
+    throw new Error(tl.loc('XdtTransformationBlockedCustomType', transformFile, attributeName, typeName));
+}
+
+function getXdtTypeName(attributeValue: string): string {
+    const argumentStartIndex = attributeValue.indexOf('(');
+    const typeName = argumentStartIndex == -1 ? attributeValue : attributeValue.substr(0, argumentStartIndex);
+    return typeName.trim();
+}
+
+function isXdtNode(node: Element | Attr, localName: string): boolean {
+    return node.namespaceURI == xdtNamespace && getLocalName(node) == localName;
+}
+
+function getLocalName(node: Element | Attr): string {
+    if (node.localName) {
+        return node.localName;
+    }
+
+    const separatorIndex = node.nodeName.indexOf(':');
+    return separatorIndex == -1 ? node.nodeName : node.nodeName.substr(separatorIndex + 1);
 }
 
 /**
